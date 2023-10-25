906336e23d47e9318677284e2f6f2412d08f4d90
Jose Carlos Jimenez <jimenezjcarlos28@gmail.com>
2023-10-24 18:28:55 -0700

    ruteando paginas user

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,7 +5,7 @@ import DataTable from "../components/DataTable.js";
 import TitleTable from "../components/TitleTable";
 import API_BASE_URL from "../config";
 
-const Dashboard = () =>{
+const Dashboard = () => {
 
   const { data } = useFetch(`${API_BASE_URL}/proyecto/get-all`);
   console.log(data);
@@ -16,7 +16,8 @@ const Dashboard = () =>{
     delete: "proyecto/delete-proyecto",
     view: '/ver-proyecto',
     edit: '/editar-proyecto'
-}
+  }
+  
   return (
     <div className="container-content">
       <div className="title">
@@ -24,16 +25,17 @@ const Dashboard = () =>{
       </div>
       <div className="content">
         <div className="title-table">
-          <TitleTable tableName='Proyectos recientes' page='/Proyectos' button='Ir a proyectos'/>
+          <TitleTable tableName='Proyectos recientes' page='/Proyectos' button='Ir a proyectos' />
         </div>
         <div>
-        {data && data.proyecto && data.proyecto.length > 0 ? (
-          <DataTable columns={columns} data={data.proyecto} pages={pages}/>
-        ) : (
-          <p>Cargando...</p>
-        )}
+          {
+          data && data.proyecto && data.proyecto.length > 0 ? (
+            <DataTable columns={columns} data={data.proyecto} pages={pages} />
+          ) : (
+            <p>Cargando...</p>
+          )}
         </div>
-        
+
       </div>
     </div>
   );
---
+++ src/pages/user/NuevoUser.js
@@ -0,0 +1,84 @@
+import React from "react";
+import "../../styles/content.css";
+import { useState } from "react";
+import API_BASE_URL from "../../config";
+
+const NuevoUser = () =>{
+    const [nombre, setNombre] = useState("");
+    const [apellidoPaterno, setApellidoPaterno] = useState("");
+    const [apellidoMaterno, setApellidoMaterno] = useState("");
+    const [numeroEmpleado, setNumeroEmpleado] = useState("");
+    const [correo, setCorreo] = useState("");
+    const [password, setPassword] = useState("Paquete2023.");
+    const [tipoUsuario, setTipoUsuario] = useState("Usuario");
+    const [sucursalId, setSucursalId] =  useState(1);
+    const [departamentoId, setDepartamentoId] =useState(1);
+
+    const handlePost = () => {
+        const data = { content:{nombre, apellidoPaterno, apellidoMaterno, numeroEmpleado, correo, password, tipoUsuario, sucursalId, departamentoId} };
+        // console.log('nombre:', nombre);
+        // console.log('fechaEntrada:', fechaEntrada);
+        // console.log('fechaSalida:', fechaSalida);
+        // console.log('userId:', userId);
+        console.log(data);
+        fetch(`${API_BASE_URL}/users/post-users`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        })
+          .then((response) => {
+            window.location.href = '/users';
+          })
+          .catch((error) => {
+            console.error("Hubo un problema al eliminar el registro:", error);
+          });
+    };
+  return (
+    <div className="container-content">
+      <div className="title">
+        <h1>Nuevo Usuario</h1>
+      </div>
+      <div className="content">
+      <form class="add-form" action="/" method="">
+            <div class="form-control">
+                <label for="nombre">Nombre</label>
+                <input type="text" name="nombre" id="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)}/>
+            </div>
+            <div class="form-control">
+                <label for="apellidoPaterno">Apellido Paterno</label>
+                <input type="text" name="apellidoPaterno" id="apellidoPaterno" value={apellidoPaterno} onChange={(e) => setApellidoPaterno(e.target.value)}/>
+            </div>
+            <div class="form-control">
+                <label for="apellidoMaterno">Apellido Materno</label>
+                <input type="text" name="apellidoMaterno" id="apellidoMaterno" value={apellidoMaterno} onChange={(e) => setApellidoMaterno(e.target.value)}/>
+            </div>
+            <div class="form-control">
+                <label for="numeroEmpleado">Numero de empleado</label>
+                <input type="text" name="numeroEmpleado" id="numeroEmpleado" value={numeroEmpleado} onChange={(e) => setNumeroEmpleado(e.target.value)}/>
+            </div>
+            <div class="form-control">
+                <label for="correo">Correo</label>
+                <input type="text" name="correo" id="correo" value={correo} onChange={(e) => setCorreo(e.target.value)}/>
+            </div>
+            <div class="form-control">
+                <label for="password">password</label>
+                <input type="text" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            <div class="form-control">
+                <label for="tipoUsuario">Tipo</label>
+                <select name="tipoUsuario" id="tipoUsuario" value={tipoUsuario} onChange={(e) => setTipoUsuario(e.target.value)}>
+                    <option value="Admin">Administrador</option>
+                    <option value="User" selected>Usuario</option>
+                </select>
+            </div>
+
+            <button class="btn" type="button" onClick={handlePost}>Crear</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default NuevoUser;
