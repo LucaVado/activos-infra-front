58e001829b9b843e47ef0cbd4582ad1fc45b4f62
Jose Carlos Jimenez <jimenezjcarlos28@gmail.com>
2023-11-06 18:05:34 -0700

    añadiendo funcion handlefinproyect y findcode a activoscctv

--- src/pages/equipocctv/NuevoActivoCctv.js
+++ src/pages/equipocctv/NuevoActivoCctv.js
@@ -3,6 +3,7 @@ import "../../styles/content.css";
 import "../../styles/inputForms.css";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
+import { showNotification } from "../../utils/notification";
 import API_BASE_URL from "../../config";
 
 const NuevoActivoCctv = () => {
@@ -17,22 +18,65 @@ const NuevoActivoCctv = () => {
     const [razon, setRazon] = useState("Incremento");
     const [codigo, setCodigo] = useState("");
     const [userId, setUserId] = useState(1);
-    const [tipoActivoId, setTipoActivoId] = useState(5);
-    const [proyectoId, setProyectoId] = useState(2);
+    const [tipoActivoId, setTipoActivoId] = useState();
+    const [proyecto, setProyecto] = useState("");
+    const [proyectoId, setProyectoId] = useState();
     const [modelo, setModelo] = useState("");
 
-    const handleFindCode= (codigo) =>{
+    const handleFindProyecto = (nombreProyecto) => {
+        console.log(nombreProyecto);
+    
+        fetch(`${API_BASE_URL}/proyecto/get-proyectoByName?proyecto=${nombreProyecto}`)
+            .then(response => response.json())
+            .then(dataProyecto => {
+                console.log(dataProyecto);
+                if (dataProyecto && dataProyecto.proyecto) {
+                    setProyecto(dataProyecto.proyecto.nombre || '');
+                    setProyectoId(dataProyecto.proyecto.id || '');
+    
+                    // Convertir fechas de ISO 8601 a formato de fecha legible
+                    if (dataProyecto.proyecto.fechaEntrada) {
+                        const fechaEntrada = new Date(dataProyecto.proyecto.fechaEntrada).toISOString().split('T')[0];
+                        setFechaEntrada(fechaEntrada);
+                    }
+                    if (dataProyecto.proyecto.fechaSalida) {
+                        const fechaSalida = new Date(dataProyecto.proyecto.fechaSalida).toISOString().split('T')[0];
+                        setFechaSalida(fechaSalida);
+                    }
+    
+                    setGuia(dataProyecto.proyecto.guia || '');
+                    setEstatus(dataProyecto.proyecto.estatus);
+                    setRazon(dataProyecto.proyecto.razon);
+                    showNotification('Proyecto encontrado con éxito', 'success');
+                } else {
+                    console.log('No se encontraron datos válidos para el proyecto.');
+                    showNotification('Ha ocurrido un error al buscar el proyecto', 'error');
+                }
+            })
+            .catch(error => {
+                console.error('Ha ocurrido un error:', error)
+                showNotification('Ha ocurrido un error', 'error');
+            });
+    };
+    
+
+    const handleFindCode = (codigo) => {
         console.log(codigo);
 
         fetch(`${API_BASE_URL}/tipo-activo/get-codigo?codigo=${codigo}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            setNombre(data.tipo.nombre);
-            setModelo(data.tipo.modelo);
-            setTipoActivoId(data.tipo.id);
-        })
-        .catch(error => console.error('Ha ocurrido un error:', error));
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setNombre(data.tipo.nombre);
+                setModelo(data.tipo.modelo);
+                setTipoActivoId(data.tipo.id);
+
+                showNotification('Código encontrado con éxito', 'success');
+            })
+            .catch(error => {
+                console.error('Ha ocurrido un error:', error)
+                showNotification('Ha ocurrido un error al buscar el código', 'error');
+            });
     }
 
     const handlePost = () => {
@@ -53,7 +97,8 @@ const NuevoActivoCctv = () => {
                 window.location.href = '/equipoCctv';
             })
             .catch((error) => {
-                console.error("Hubo un problema al eliminar el registro:", error);
+                console.error("Hubo un problema al crear el activo:", error);
+                showNotification('Ha ocurrido un error al crear el activo', 'error');
             });
     };
     return (
@@ -62,17 +107,24 @@ const NuevoActivoCctv = () => {
                 <h1>Nuevo Activo CCTV</h1>
             </div>
             <div className="content">
-                <div className="new-link" style={{paddingLeft: '22px'}}>
+                <div className="new-link" style={{ paddingLeft: '22px' }}>
                     <NavLink to='/nuevo-modelo' className="button">
                         <span><h4>+Agregar modelo</h4></span>
                     </NavLink>
                 </div>
                 <form class="add-form" action="/" method="">
-                <div class="form-control">
+                    <div class="form-control">
                         <label for="codigo">Codigo</label>
                         <div>
-                        <input type="text" name="codigo" id="codigo" value={codigo} onChange={(e) => setCodigo(e.target.value)} />
-                        <button class="btn-buscar-modelo" type="button" onClick={() => handleFindCode(codigo)}>Buscar codigo</button>
+                            <input type="text" name="codigo" id="codigo" value={codigo} onChange={(e) => setCodigo(e.target.value)} />
+                            <button class="btn-buscar-modelo" type="button" onClick={() => handleFindCode(codigo)}>Buscar codigo</button>
+                        </div>
+                    </div>
+                    <div class="form-control">
+                        <label for="proyecto">Proyecto</label>
+                        <div>
+                            <input type="text" name="proyecto" id="proyecto" value={proyecto} onChange={(e) => setProyecto(e.target.value)} />
+                            <button class="btn-buscar-modelo" type="button" onClick={() => handleFindProyecto(proyecto)}>Buscar proyecto</button>
                         </div>
                     </div>
                     <div class="form-control">
@@ -80,17 +132,13 @@ const NuevoActivoCctv = () => {
                         <input type="text" name="modelo" id="modelo" value={modelo} onChange={(e) => setModelo(e.target.value)} />
                     </div>
                     <div class="form-control">
-                        <label for="numeroSerie">Numero de serie</label>
-                        <input type="text" name="numeroSerie" id="numeroSerie" value={numeroSerie} onChange={(e) => setNumeroSerie(e.target.value)} />
-                    </div>
-                    <div class="form-control">
                         <label for="nombre">Nombre</label>
                         <input type="text" name="nombre" id="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
                     </div>
                     <div class="form-control">
-                        <label for="numeroActivo">Numero de Activo</label>
-                        <input type="text" name="numeroActivo" id="numeroActivo" value={numeroActivo} onChange={(e) => setNumeroActivo(e.target.value)} />
-                    </div>
+                        <label for="numeroSerie">Numero de serie</label>
+                        <input type="text" name="numeroSerie" id="numeroSerie" value={numeroSerie} onChange={(e) => setNumeroSerie(e.target.value)} />
+                    </div>                    
                     <div class="form-control">
                         <label for="fechaEntrada">Fecha de Entrada</label>
                         <input type="date" name="fechaEntrada" id="fechaEntrada" value={fechaEntrada} onChange={(e) => setFechaEntrada(e.target.value)} />
@@ -108,14 +156,6 @@ const NuevoActivoCctv = () => {
                         </select>
                     </div>
                     <div class="form-control">
-                        <label for="folio">folio</label>
-                        <input type="text" name="folio" id="folio" value={folio} onChange={(e) => setFolio(e.target.value)} />
-                    </div>
-                    <div class="form-control">
-                        <label for="guia">guia</label>
-                        <input type="text" name="guia" id="guia" value={guia} onChange={(e) => setGuia(e.target.value)} />
-                    </div>
-                    <div class="form-control">
                         <label for="razon">razon</label>
                         <select name="razon" id="razon" value={razon} onChange={(e) => setRazon(e.target.value)}>
                             <option value="Incremento">Incremento</option>
@@ -123,6 +163,19 @@ const NuevoActivoCctv = () => {
                             <option value="Apertura" >Apertura</option>
                         </select>
                     </div>
+                    <div class="form-control">
+                        <label for="numeroActivo">Numero de Activo</label>
+                        <input type="text" name="numeroActivo" id="numeroActivo" value={numeroActivo} onChange={(e) => setNumeroActivo(e.target.value)} />
+                    </div>
+                    <div class="form-control">
+                        <label for="folio">folio</label>
+                        <input type="text" name="folio" id="folio" value={folio} onChange={(e) => setFolio(e.target.value)} />
+                    </div>
+                    <div class="form-control">
+                        <label for="guia">guia</label>
+                        <input type="text" name="guia" id="guia" value={guia} onChange={(e) => setGuia(e.target.value)} />
+                    </div>
+                    
                     <div class="form-control"></div>
 
                     <div className="form-button"><button class="btn" type="button" onClick={handlePost}>Crear</button></div>
