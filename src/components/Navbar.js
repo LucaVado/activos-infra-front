afd6ea269155426976614c95c05f4a733416ae7b
Luis Carlos Valdez Dominguez <Luca_Vado@hotmail.com>
2024-04-05 12:46:51 -0700

    Se ajusto el tamaño del icono logout a 100% height

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,14 +21,14 @@ const Navbar = () => {
     }
     return (
         <div className="navbar">
-            {/*
+
             <div className="logo-navbar"><img src="/images/icons/paquetexpress-logo-navbar.svg"></img></div>
             <div className="logout-navbar">
                 <button className="logout-button" onClick={handleLogout}>
                     <img className="logout-img" src="/images/icons/icono-logout2.svg" title="Cerrar Sesión" alt="View" />
                 </button>
             </div>
-            */}
+
         </div>
     );
 }
