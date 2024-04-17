82dfdeb40000f86c4f1c492541fc3582d0493ad6
Luis Carlos Valdez Dominguez <Luca_Vado@hotmail.com>
2024-04-17 12:27:17 -0700

    Se ajusto el tamaño de la tabla, ajustando la ultima columna y header que pertence a actions, los botones de liminar, ver y editar, de esta forma aunque la pantalla sea chica como telefono esta siempre permanece visible

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,39 +1,44 @@
 import React from "react";
 import ColumnActions from "./ColumnActions";
 import "../styles/dataTable.css";
+
 const DataTable = ({ columns, data, pages }) => {
-
-
   return (
-    <div className="table-container">
-      <table className="data-table">
-        <thead>
-          <tr className="table-header">
-            {columns.map((column, index) => (
-              <th className="header-cell" key={index}>{column}</th>
-            ))}
-            <th className="header-cell">Actions</th>
-          </tr>
-        </thead>
-        <tbody className="table-body">
-          {data.map((item, index) => (
-            <tr key={index} className={index % 2 === 0 ? 'table-row even' : 'table-row odd'}>
-              {columns.map((column, i) => (
-                <td key={i} className="table-cell">
-                  {item[column]}
-                </td>
+      <div className="table-container">
+        <div className="table-scroll">
+          <table className="data-table">
+            <thead>
+            <tr className="table-header">
+              {columns.map((column, index) => (
+                  <th className="header-cell" key={index}>
+                    {column}
+                  </th>
               ))}
-              <td className="table-cell">
-                <ColumnActions
-                  item={item}
-                  pages={pages}
-                />
-              </td>
+              <th className="header-cell fixed-cell">Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+            </thead>
+            <tbody className="table-body">
+            {data.map((item, index) => (
+                <tr
+                    key={index}
+                    className={index % 2 === 0 ? "table-row even" : "table-row odd"}
+                >
+                  {columns.map((column, i) => (
+                      <td key={i} className="table-cell">
+                        {item[column]}
+                      </td>
+
+                  ))}
+                  <td className="table-cell fixed-cell-content">
+                    <ColumnActions item={item} pages={pages} />
+
+                  </td>
+                </tr>
+            ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
   );
 };
 
