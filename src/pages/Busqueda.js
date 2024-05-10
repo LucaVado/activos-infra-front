88b845a329a2c3751cf1424570c9c8cf3c4d19be
Luis Carlos Valdez Dominguez <Luca_Vado@hotmail.com>
2024-05-10 12:01:48 -0700

    Se agrego cath para el error en caso de no encontrar activos

--- src/pages/Busqueda.js
+++ src/pages/Busqueda.js
@@ -5,6 +5,7 @@ import API_BASE_URL from "../config";
 const Busqueda = () => {
     const [activos, setActivos] = useState([]);
     const [cargando, setCargando] = useState(true);
+    const [error, setError] = useState(null);
     const params = useParams();
 
     useEffect(() => {
@@ -15,21 +16,29 @@ const Busqueda = () => {
         try {
             const response = await fetch(`${API_BASE_URL}/activos/buscar/${params.busqueda}`);
             if (!response.ok) {
-                throw new Error('Error al obtener los activos');
+                throw new Error('No se encontraron activos');
             }
             const data = await response.json();
             setActivos(data.activos);
             setCargando(false);
+            setError(null);
         } catch (error) {
-            console.error('Error:', error);
+            console.error('Error:', error.message);
+            setError('No se encontraron activos');
             setCargando(false);
+            setActivos([]);
         }
     };
+
 
     return (
         <div>
             {cargando ? (
                 <div>Cargando...</div>
+            ) : error ? (
+                <div>{error}</div>
+            ) : activos.length === 0 ? (
+                <div>No se encontraron activos.</div>
             ) : (
                 <div>
                     <h2>Activos encontrados:</h2>
@@ -46,6 +55,7 @@ const Busqueda = () => {
                                 <p>Guía: {activo.guia}</p>
                                 <p>Estatus: {activo.estatus}</p>
                                 <p>Razón: {activo.razon}</p>
+                                {/* Agrega más propiedades de activo según sea necesario */}
                             </li>
                         ))}
                     </ul>
