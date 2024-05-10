7e82bf9114c32be27bf51883933d8fffed3ac1d6
Luis Carlos Valdez Dominguez <Luca_Vado@hotmail.com>
2024-05-10 09:51:19 -0700

    Se utiliza el uso de useParams, para tomar un pedazo de la url, para tomarlo como busqueda

--- src/pages/Busqueda.js
+++ src/pages/Busqueda.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {useParams} from "react-router-dom";
 
 export const Busqueda = () => {
+    const [activos, setActivos] = useState([]);
+    const [cargando, setCargando] = useState(true);
+    const params = useParams();
+
+    useEffect(() => {
+        console.log(params)
+    }, []);
+
     return (
         <div>Busqueda</div>
     )
